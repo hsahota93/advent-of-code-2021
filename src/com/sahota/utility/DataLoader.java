c7288a3551d25f834687d69bda22170c6bd82796
Harman Sahota <harman.sahota93@gmail.com>
2021-12-12 23:20:32 -0500

    day 2 prep; refactoring

---
+++ src/com/sahota/utility/DataLoader.java
@@ -0,0 +1,36 @@
+package com.sahota.utility;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// The intent of this glass is to easily pull in data provided by the advent of code
+// daily challenges. Usually some sort of file
+public class DataLoader {
+
+    public List<Integer> getDay1Input(String fileName) {
+
+        ArrayList<Integer> data = new ArrayList<>();
+
+        try {
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+
+            while (myReader.hasNextLine()) {
+
+                Integer integer = Integer.valueOf(myReader.nextLine());
+                data.add(integer);
+            }
+
+            myReader.close();
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Couldn't find the file...");
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+}
